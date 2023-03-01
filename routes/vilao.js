0e03888d06aa44687df23acc6265cc8d6a0e9e5c
lu058294 <lu058294@intelbras.com.br>
2023-03-01 20:53:34 -0300

    novas rotas adicionadas

--- routes/vilao.js
+++ routes/vilao.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const controller = require("./../controllers/vilao")
 
 router.post("/cadastrar-vilao", controller.post)
+router.post("/fundir", controller.fundir)
 
 module.exports = router
